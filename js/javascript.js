85fd37766b312fadfaaa81065eb14caf46fc184e
Josemilson Júnior <josemilson.junior@a.ficr.edu.br>
2020-05-17 23:48:46 -0300

    Inclusão da página Lançamento de Cursos

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,14 @@ $(document).ready(function(){
   $('.slider').slider();
 });
 
+$(document).ready(function(){
+  $('.timepicker').timepicker();
+});
+
+$(document).ready(function(){
+  $('.datepicker').datepicker();
+});
+
 M.AutoInit();
 
 
